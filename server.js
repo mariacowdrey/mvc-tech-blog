653ae7ab31be6154331a5d19f64a735058b50f6d
Maria Cowdrey <mariacowdrey@yahoo.com>
2021-11-24 00:38:53 -0800

    create api routes for user model

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-const path = require('path');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
